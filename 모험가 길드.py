5c980e4a685741103776923993d922fd09e0af2a
Jo Young Seok <zkftpfm@gmail.com>
2025-03-03 21:15:15 +0900

    Update 모험가 길드.py

--- 모험가 길드.py
+++ 모험가 길드.py
@@ -1,7 +1,18 @@
 # 일단 입력받기
 n = input()
-heros = list(map(int, input().split()))
+heroes = list(map(int, input().split()))
 
 # 단순히 생각하면 가장 공포도가 적은 사람으로 가장 적은수로 그룹을 만드는것을 반복하면 최대로 많은 그룹이 떠난다.
 # 가장 적은 공포도 부터 다음 적은것까지 계산하려면 오름차순으로 리스트를 정렬하면 된다.
-heros.sort()
+heroes.sort()
+
+result = 0 # 결과값 마지막 그룹수
+count = 0 # 현재 세고있는 그룹의 인원수
+
+for a in heroes;
+  count += 1
+  if count = a;
+    result += 1
+    count = 0
+
+print(result)
